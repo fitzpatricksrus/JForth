a065797e2225a46890e1aaebc79c3bdd180c8c0a
fitzpatricksrus <junk@fitzpatircksr.us>
2016-07-18 23:08:36 -0700

    Started converting to reflection based dictionaries

--- src/us/cownet/jforth/BooleanConstant.java
+++ src/us/cownet/jforth/BooleanConstant.java
@@ -8,65 +8,50 @@ public class BooleanConstant extends DataWord<Boolean> {
 		super(value);
 	}
 
+	private static boolean pop(ExecutionContext context) {
+		return ((DataWord<Boolean>) context.pop()).getValue();
+	}
+
 	//--------------------------
 	// Vocabulary
 
-	@Override
-	protected Vocabulary constructVocabulary() {
-		return super.constructVocabulary()
-				.addWord("!", new BooleanNot())
-				.addWord("== ", new BooleanEquals())
-				.addWord("!=", new BooleanNotEquals())
-				.addWord("&&", new BooleanAnd())
-				.addWord("||", new BooleanOr())
-				.addWord("^", new BooleanXor());
+	@AlternateName(name = "!")
+	public static void not(ExecutionContext context) {
+		context.push((!pop(context)) ? TRUE : FALSE);
 	}
 
-	public static class BooleanNot extends UnaryOperator<Boolean> {
-		@Override
-		protected Word operate(Boolean value) {
-			// ( BooleanConstant - BooleanConstant )
-			return (!value) ? TRUE : FALSE;
-		}
+	@AlternateName(name = "==")
+	public static void equals(ExecutionContext context) {
+		boolean v1 = pop(context);
+		boolean v2 = pop(context);
+		context.push((v1 == v2) ? TRUE : FALSE);
 	}
 
-	public static class BooleanEquals extends BinaryOperator<Boolean> {
-		@Override
-		protected Word operate(Boolean v1, Boolean v2) {
-			// ( BooleanConstant, BooleanConstant - BooleanConstant )
-			return (v1 == v2) ? TRUE : FALSE;
-		}
+	@AlternateName(name = "!=")
+	public static void notEquals(ExecutionContext context) {
+		boolean v1 = pop(context);
+		boolean v2 = pop(context);
+		context.push((v1 != v2) ? TRUE : FALSE);
 	}
 
-	public static class BooleanNotEquals extends BinaryOperator<Boolean> {
-		@Override
-		protected Word operate(Boolean v1, Boolean v2) {
-			// ( BooleanConstant, BooleanConstant - BooleanConstant )
-			return (v1 == v2) ? TRUE : FALSE;
-		}
+	@AlternateName(name = "||")
+	public static void or(ExecutionContext context) {
+		boolean v1 = pop(context);
+		boolean v2 = pop(context);
+		context.push((v1 || v2) ? TRUE : FALSE);
 	}
 
-	public static class BooleanOr extends BinaryOperator<Boolean> {
-		@Override
-		protected Word operate(Boolean v1, Boolean v2) {
-			// ( BooleanConstant, BooleanConstant - BooleanConstant )
-			return (v1 || v2) ? TRUE : FALSE;
-		}
+	@AlternateName(name = "&&")
+	public static void and(ExecutionContext context) {
+		boolean v1 = pop(context);
+		boolean v2 = pop(context);
+		context.push((v1 && v2) ? TRUE : FALSE);
 	}
 
-	public static class BooleanAnd extends BinaryOperator<Boolean> {
-		@Override
-		protected Word operate(Boolean v1, Boolean v2) {
-			// ( BooleanConstant, BooleanConstant - BooleanConstant )
-			return (v1 && v2) ? TRUE : FALSE;
-		}
-	}
-
-	public static class BooleanXor extends BinaryOperator<Boolean> {
-		@Override
-		protected Word operate(Boolean v1, Boolean v2) {
-			// ( BooleanConstant, BooleanConstant - BooleanConstant )
-			return (v1 ^ v2) ? TRUE : FALSE;
-		}
+	@AlternateName(name = "^")
+	public static void xor(ExecutionContext context) {
+		boolean v1 = pop(context);
+		boolean v2 = pop(context);
+		context.push((v1 ^ v2) ? TRUE : FALSE);
 	}
 }
--- src/us/cownet/jforth/DataWord.java
+++ src/us/cownet/jforth/DataWord.java
@@ -14,26 +14,4 @@ public abstract class DataWord<T> extends Word {
 	protected void setValue(T newValue) {
 		value = newValue;
 	}
-
-	public static abstract class UnaryOperator<T> extends Word {
-		@Override
-		public void execute(ExecutionContext context) {
-			DataWord<T> v = (DataWord<T>) context.pop();
-			context.push(operate(v.value));
-		}
-
-		protected abstract Word operate(T value);
-	}
-
-	public static abstract class BinaryOperator<T> extends Word {
-		@Override
-		public void execute(ExecutionContext context) {
-			DataWord<T> v1 = (DataWord<T>) context.pop();
-			DataWord<T> v2 = (DataWord<T>) context.pop();
-			context.push(operate(v1.value, v2.value));
-		}
-
-		protected abstract Word operate(T v1, T v2);
-	}
-
 }
